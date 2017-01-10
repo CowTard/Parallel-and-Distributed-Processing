6da00e65b97b691bdaa7a888cf515b449f036e42
Cowtard <ei12109@fe.up.pt>
2017-01-10 17:39:42 +0100

    Sieve done.

--- Sieve of Eratosthenes/main.c
+++ Sieve of Eratosthenes/main.c
@@ -1,28 +1,54 @@
 #include<stdio.h>
 #include<omp.h>
+#include<math.h>
 
-#define SIZE_OF_INITIAL_LIST 50
+#define SIZE_OF_INITIAL_LIST 120
 
 int main()
 {
-	omp_init_lock(&lock);
-	srand(time(NULL));
-
     // Create and populate the sieve's list
-    int sieve_list[SIZE_OF_INITIAL_LIST];
+    int sieve_list[SIZE_OF_INITIAL_LIST], primes[SIZE_OF_INITIAL_LIST];
+    int check_limit = floor(sqrt(SIZE_OF_INITIAL_LIST));
 
+    // Starting the arrays. Prime array has 1 if the index is not prime, or 0 otherwise.
     for(int i = 0; i < SIZE_OF_INITIAL_LIST; i++)
     {
-        sieve_list[i] = i + 1;
+        sieve_list[i] = i;
+        primes[i] = -1; // Not yet seen
     }
+
+    /*
+        1. Choose the next candidate to be prime
+        2. Use parallel process to calculate the elements to crossout.
+    */
+
 
 	omp_set_num_threads(4);
 
-    #pragma omp parallel for firstprivate(a)
-		for(i=0;i<10;i++)
+    #pragma omp parallel for
+        for(int i = 2; i < check_limit; i++)
 		{
-			printf("Thread %d a=%d\n",omp_get_thread_num(),a);
-			a++;
+            if (primes[i] != -1)
+            {
+                continue;
+            }
+            else
+            {
+                primes[i] = 0;
+                for (size_t t = i * 2; t < SIZE_OF_INITIAL_LIST; t += i) {
+                    primes[t] = 1;
+                }
+            }
 		}
 
+    printf("The primes until %d are [", SIZE_OF_INITIAL_LIST);
+    for (size_t prime_cand = 0; prime_cand < SIZE_OF_INITIAL_LIST; prime_cand++) {
+
+        if (prime_cand < 2) continue; // 0 and 1 are not primes.
+        if (primes[prime_cand] == 0 || primes[prime_cand] == -1)
+        {
+            printf("%d ", sieve_list[prime_cand]);
+        }
+    }
+    printf("]\n");
 }
