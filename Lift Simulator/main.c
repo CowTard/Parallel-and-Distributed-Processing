53aa2ab507ea3373cf6c1697bba60c3b087b2ab9
Cowtard <ei12109@fe.up.pt>
2017-01-12 17:39:36 +0100

    Wait for a request from a floor.

--- Lift Simulator/main.c
+++ Lift Simulator/main.c
@@ -8,6 +8,13 @@
 #define MAX_NUMBER_OF_PEOPLE_IN_ELEVATOR 10
 #define MAX_NUMBER_OF_PEOPLE_WAITING 5
 
+typedef struct {
+	int desired_floor;
+} Person;
+
+/*
+	Tag 0: used for requests
+*/
 int main(int argc, char** argv)
 {
     // Create seed for random
@@ -21,13 +28,19 @@ int main(int argc, char** argv)
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
 
     if (rank == 0) {
+
+		Person* people_elevator[MAX_NUMBER_OF_PEOPLE_IN_ELEVATOR];
+		int number_people_elevator = 0;
+		int current_floor = 0, desired_floor = 0;
+
 		/*
-			- Wait for a request from a floor. []
+			- Wait for a request from a floor. [x]
 			- Change floors after a requets. []
 			- Open doors in each floor a person gets out. []
 			- Open door when reach the desired floor. []
 		*/
-		
+
+		MPI_Recv(&desired_floor, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
 	} else {
 		/*
 			- Create new people. []
